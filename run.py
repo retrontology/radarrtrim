2959e415e3a34491bac8d17390f36ea728c7daf8
Retrontology <retrontology@hotmail.com>
2022-02-19 23:59:18 -0600

    remove watched movies from plex

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from os import remove
 from radarr_client import radarr_client
 from plexapi.myplex import PlexServer, NotFound
-import datetime
 import ssl
 
 RADARR_API_KEY = 'your radarr api key'
@@ -13,10 +12,9 @@ PLEX_TOKEN = 'your plex auth token'
 PLEX_BASE_URL = 'your plex.direct url'
 
 def patch_ssl():
-    
     ssl.match_hostname = lambda cert, hostname: True
 
-def remove_watched_movies(plex:PlexServer, radarr:radarr_client):
+def remove_watched_movies_from_radarr(plex:PlexServer, radarr:radarr_client):
     movies = radarr.get_movies()
     for movie in movies:
         title = movie['title']
@@ -33,7 +31,24 @@ def remove_watched_movies(plex:PlexServer, radarr:radarr_client):
                 except NotFound:
                     pass
         if isWatched == True:
+            movie.delete()
             radarr.delete_movie(movie['id'], deleteFiles=True)
+
+def remove_watched_movies_from_plex(plex:PlexServer):
+    movies = plex.library.section('Movies').all()
+    for movie in movies:
+        if movie.isWatched:
+            deleted = False
+            while not deleted:
+                try:
+                    movie.delete()
+                    deleted = True
+                except Exception as e:
+                    print(e)
+
+def remove_watched_movies(plex:PlexServer, radarr:radarr_client):
+    remove_watched_movies_from_radarr(plex, radarr)
+    remove_watched_movies_from_plex(plex)
 
 def main():
     patch_ssl()
@@ -41,10 +56,6 @@ def main():
     # Clients
     radarr = radarr_client(RADARR_IP, RADARR_PORT, RADARR_API_PATH, RADARR_API_KEY)
     plex = PlexServer(PLEX_BASE_URL, PLEX_TOKEN)
-
-    # Get Movies
-    #movies = radarr.get_movies_added_before(datetime.datetime.now() - datetime.timedelta(weeks=24))
-    movies = radarr.get_movies()
 
     # Trim
     remove_watched_movies(plex, radarr)
